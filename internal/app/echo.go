08d84bb1a9637709c1df8e781510bf7b0353cd77
imantung <iman.tung@gmail.com>
2024-09-19 07:02:05 +0700

    custom error page

--- internal/app/echo.go
+++ internal/app/echo.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/imantung/fullstack-golang-example/internal/app/controller"
 	"github.com/imantung/fullstack-golang-example/internal/app/infra/di"
 	"github.com/labstack/echo/v4"
@@ -15,9 +17,22 @@ func NewEcho(
 	e := echo.New()
 	e.Renderer = NewTemplateRegistry()
 
+	e.HTTPErrorHandler = HTTPErrorHandler
+
 	e.Static("/dist", "web/dist")
 	e.GET("/", webCntrl.HomePage)
 	e.GET("/about", webCntrl.AboutPage)
 
 	return e
+}
+
+func HTTPErrorHandler(err error, c echo.Context) {
+	he, ok := err.(*echo.HTTPError)
+	if !ok {
+		he = &echo.HTTPError{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+	}
+	c.Render(http.StatusOK, "error.html", he)
 }
